8f800ffe3a8e8cc3fee01f73dc8e8ab10e3188b6
Vyacheslav Puchkin <v.puchkin@bk.ru>
2017-05-24 17:04:02 +0500

    Очищен репозиторий.

--- js/html5.js
+++
@@ -1,7 +0,0 @@
-document.createElement('section');
-document.createElement('header');
-document.createElement('footer');
-document.createElement('nav');
-document.createElement('aside');
-document.createElement('article');
-document.createElement('mark');
